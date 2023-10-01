aa0dec7ca7c835c7e9f75c4a6b05f3495f320737
Swank <willmadedesign@gmail.com>
2023-10-01 16:52:27 -0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("                                  ");
+        Console.WriteLine(" ~~~~~~~ Hi I am doBOt! :) ~~~~~~~");
+        Console.WriteLine("                                   ");
+        Console.WriteLine(" ================================= ");
+        Console.WriteLine("     8-BIT TO_DO LIST TERMINAL      ");
+        Console.WriteLine("                                    ");
+        Console.WriteLine("       INPUT TASK => 'ENTER'   ");
+        Console.WriteLine(" ================================= ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("              TASKS                ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("                                   ");
+        Console.WriteLine("        Press E N T E R            ");
+        string? task = Console.ReadLine();
+
+
+        //Console.WriteLine(${task});
+    }
+}
